8a2bcdb169d7806684c038554327eb5d615bb406
RicholeYu <richole.yu@qq.com>
2022-09-09 17:38:02 +0800

    .

--- src/schedule/service.ts
+++ src/schedule/service.ts
@@ -1,11 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { Spot } from '@binance/connector';
+import sendEmail from '@dfyu/mailer';
+
+const { BINANCE_KEY, BINANCE_SECURE } = process.env;
+const client = new Spot(BINANCE_KEY, BINANCE_SECURE);
 
 @Injectable()
 export default class Service {
-  @Cron(CronExpression.EVERY_10_SECONDS)
-  getHello(): string {
-    console.log('Hello Richole');
-    return 'Hello World!';
+  constructor() {
+    this.getWindowTicker();
+  }
+
+  @Cron(CronExpression.EVERY_HOUR)
+  async getWindowTicker() {
+    const response = await client.rollingWindowTicker(
+      '',
+      ['BTCUSDT', 'ETHUSDT'],
+      {
+        windowSize: '1h',
+      },
+    );
+    const ETH = response.data.find((item) => item.symbol === 'ETHUSDT');
+    const BTC = response.data.find((item) => item.symbol === 'BTCUSDT');
+
+    const pricePercent = (item) =>
+      item.priceChangePercent.startsWith('-')
+        ? `⬇️${+(+item.priceChangePercent.slice(1)).toFixed(2)}%️`
+        : `⬆️${+(+item.priceChangePercent).toFixed(2)}%`;
+
+    sendEmail({
+      to: 'richoleyu@126.com',
+      subject: '一小时币圈快报',
+      html: [
+        `<p>ETH: ${~~ETH.lastPrice}, ${pricePercent(ETH)}</p>`,
+        `<p>BTC: ${~~BTC.lastPrice}, ${pricePercent(BTC)}</p>`,
+      ].join(''),
+    });
   }
 }
