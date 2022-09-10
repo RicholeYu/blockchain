c01d70d5998b1b8753a1561cd44ccb6c215b4ddf
RicholeYu <richole.yu@qq.com>
2022-09-10 09:36:48 +0800

    .

--- src/schedule/service.ts
+++ src/schedule/service.ts
@@ -6,28 +6,38 @@ import sendEmail from '@dfyu/mailer';
 const { BINANCE_KEY, BINANCE_SECURE } = process.env;
 const client = new Spot(BINANCE_KEY, BINANCE_SECURE);
 
+async function getTicker(windowSize) {
+  const response = await client.rollingWindowTicker(
+    '',
+    ['BTCUSDT', 'ETHUSDT'],
+    {
+      windowSize,
+    },
+  );
+  const ETH = response.data.find((item) => item.symbol === 'ETHUSDT');
+  const BTC = response.data.find((item) => item.symbol === 'BTCUSDT');
+
+  return {
+    ETH,
+    BTC,
+  };
+}
+
+function pricePercent(item) {
+  return item.priceChangePercent.startsWith('-')
+    ? `⬇️${+(+item.priceChangePercent.slice(1)).toFixed(2)}%️`
+    : `⬆️${+(+item.priceChangePercent).toFixed(2)}%`;
+}
 @Injectable()
 export default class Service {
   constructor() {
-    this.getWindowTicker();
+    this.getEveryHourTicker();
   }
 
+  // 每小时固定推送
   @Cron(CronExpression.EVERY_HOUR)
-  async getWindowTicker() {
-    const response = await client.rollingWindowTicker(
-      '',
-      ['BTCUSDT', 'ETHUSDT'],
-      {
-        windowSize: '1h',
-      },
-    );
-    const ETH = response.data.find((item) => item.symbol === 'ETHUSDT');
-    const BTC = response.data.find((item) => item.symbol === 'BTCUSDT');
-
-    const pricePercent = (item) =>
-      item.priceChangePercent.startsWith('-')
-        ? `⬇️${+(+item.priceChangePercent.slice(1)).toFixed(2)}%️`
-        : `⬆️${+(+item.priceChangePercent).toFixed(2)}%`;
+  async getEveryHourTicker() {
+    const { ETH, BTC } = await getTicker('1h');
 
     sendEmail({
       to: 'richoleyu@126.com',
@@ -37,5 +47,24 @@ export default class Service {
         `<p>BTC: ${~~BTC.lastPrice}, ${pricePercent(BTC)}</p>`,
       ].join(''),
     });
+  }
+
+  // 15分钟内涨跌幅超1%
+  @Cron(CronExpression.EVERY_MINUTE)
+  async getShortTicker() {
+    const { ETH, BTC } = await getTicker('15m');
+    const ethPrice = +ETH.priceChangePercent;
+    const btcPrice = +BTC.priceChangePercent;
+
+    if (ethPrice > 1.0 || ethPrice < 1.0 || btcPrice > 1.0 || btcPrice < 1.0) {
+      sendEmail({
+        to: 'richoleyu@126.com',
+        subject: '过去15分钟涨跌幅超1%',
+        html: [
+          `<p>ETH: ${~~ETH.lastPrice}, ${pricePercent(ETH)}</p>`,
+          `<p>BTC: ${~~BTC.lastPrice}, ${pricePercent(BTC)}</p>`,
+        ].join(''),
+      });
+    }
   }
 }
