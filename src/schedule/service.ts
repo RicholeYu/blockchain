3b7c67a3919585ebd38a0464b7e35cdadf205c6e
RicholeYu <richole.yu@qq.com>
2022-09-11 22:24:20 +0800

    .

--- src/schedule/service.ts
+++ src/schedule/service.ts
@@ -52,64 +52,88 @@ const sendEmailBySubject = (subject, item: string | Ticker) => {
   }
 };
 
+function formatTime(time) {
+  const nums = time.split('');
+  const unit = {
+    m: '分钟',
+    h: '小时',
+    d: '天',
+  }[nums.pop()];
+  return nums.join('') + unit;
+}
+
 export default function (coin: string): any {
   @Injectable()
   class Service {
-    continueLongTimes = 0;
-    continueShortTimes = 0;
-    last15MinPrice = 0;
-    continueShortArr = [];
-    continueLongArr = [];
-    last5MinPrice = null;
-    constructor() {
-      this.getShortTicker();
-    }
+    lastPrice = {};
+    continueShortTimes = {};
+    continueLongTimes = {};
+    continueShortArr = {};
+    continueLongArr = {};
 
     // 每小时固定推送
     @Cron(CronExpression.EVERY_HOUR)
     async getEveryHourTicker() {
-      const item = await getTicker(coin, '1h');
+      // 每小时交易量增幅
+      this.getShortTicker('1h');
+      this.getImportantTicker('1h');
+      this.getContinueTicker('1h');
 
+      const item = await getTicker(coin, '1h');
       sendEmailBySubject(`一小时快报(${coin})`, item);
     }
 
-    // 过去5分钟交易量增幅超过30%
     @Cron(CronExpression.EVERY_5_MINUTES)
-    async getShortTicker() {
-      const item = await getTicker(coin, '5m');
-
-      if (this.last5MinPrice) {
-        Logger.log(`${item.quoteVolume}  ${this.last5MinPrice.quoteVolume}`);
-        const precent =
-          (item.quoteVolume / this.last5MinPrice.quoteVolume - 1) * 100;
-        if (precent > 30) {
-          sendEmailBySubject(`过去5分钟交易量大增${precent.toFixed(1)}%`, item);
-        }
-      }
-
-      this.last5MinPrice = item;
+    async every5MinTask() {
+      // 过去5分钟交易量增幅
+      this.getShortTicker('5m');
     }
 
     // 15分钟时间段连续涨跌幅
     @Cron('0 */15 * * * *')
-    async getContinue15MinTicker() {
-      const item = await getTicker(coin, '15m');
+    async every15MinTask() {
+      this.getImportantTicker('15m');
+      this.getContinueTicker('15m');
+    }
+
+    // 交易量增幅
+    async getShortTicker(time) {
+      const item = await getTicker(coin, time);
+
+      if (this.lastPrice[time]) {
+        const precent =
+          (item.quoteVolume / this.lastPrice[time].quoteVolume - 1) * 100;
+        if (precent > 100) {
+          sendEmailBySubject(
+            `过去${formatTime(time)}交易量大增${precent.toFixed(1)}%(${coin})`,
+            item,
+          );
+        }
+      }
+
+      this.lastPrice[time] = item;
+    }
+
+    async getContinueTicker(time) {
+      const item = await getTicker(coin, time);
       const price = +item.priceChangePercent;
 
       if (price > 0) {
-        this.continueShortTimes = 0;
-        this.continueShortArr = [];
-        this.continueLongTimes++;
-        this.continueLongArr.push(item);
+        this.continueShortTimes[time] = 0;
+        this.continueShortArr[time] = [];
+        this.continueLongTimes[time] = this.continueLongTimes[time] || 0;
+        this.continueLongArr[time] = this.continueLongArr[time] || [];
+        this.continueLongTimes[time]++;
+        this.continueLongArr[time].push(item);
 
-        if (this.continueLongTimes >= 3) {
-          const total = this.continueLongArr.reduce(
+        if (this.continueLongTimes[time] >= 3) {
+          const total = this.continueLongArr[time].reduce(
             (total, current) => total + +current.priceChangePercent,
             0,
           );
           sendEmailBySubject(
-            `15分钟时间段已连续上涨超过${this.continueLongTimes}次(${coin})`,
-            `从${~~this.continueLongArr[0]
+            `15分钟时间段已连续上涨超过${this.continueLongTimes[time]}次(${coin})`,
+            `从${~~this.continueLongArr[time][0]
               .lastPrice}上涨到${~~item.lastPrice}, 共计上涨${total.toFixed(
               2,
             )}%`,
@@ -118,19 +142,21 @@ export default function (coin: string): any {
       }
 
       if (price < 0) {
-        this.continueLongTimes = 0;
-        this.continueLongArr = [];
-        this.continueShortTimes++;
-        this.continueShortArr.push(item);
+        this.continueLongTimes[time] = 0;
+        this.continueLongArr[time] = [];
+        this.continueShortTimes[time] = this.continueShortTimes[time] || 0;
+        this.continueShortArr[time] = this.continueShortArr[time] || [];
+        this.continueShortTimes[time]++;
+        this.continueShortArr[time].push(item);
 
-        if (this.continueShortTimes >= 3) {
-          const total = this.continueLongArr.reduce(
+        if (this.continueShortTimes[time] >= 3) {
+          const total = this.continueShortArr[time].reduce(
             (total, current) => total + +current.priceChangePercents.slice(1),
             0,
           );
           sendEmailBySubject(
-            `15分钟时间段已连续下跌超过${this.continueShortTimes}次(${coin})`,
-            `从${~~this.continueLongArr[0]
+            `15分钟时间段已连续下跌超过${this.continueShortTimes[time]}次(${coin})`,
+            `从${~~this.continueShortArr[time][0]
               .lastPrice}下跌到${~~item.lastPrice}, 共计下跌${total.toFixed(
               2,
             )}%`,
@@ -139,33 +165,38 @@ export default function (coin: string): any {
       }
     }
 
-    // 插针或连续行情
-    @Cron('0 */15 * * * *')
-    async getImportantTicker() {
-      const item = await getTicker(coin, '15m');
+    async getImportantTicker(time) {
+      const item = await getTicker(coin, time);
       const price = +item.priceChangePercent;
+      const lastPrice = this.lastPrice[time];
 
       if (price > 1) {
-        if (this.last15MinPrice > 1) {
-          sendEmailBySubject(`15分钟时间段已出现连续大波动上涨行情`, item);
+        if (lastPrice && +lastPrice.priceChangePercent > 1) {
+          sendEmailBySubject(
+            `${formatTime(time)}时间段已出现连续大波动上涨行情`,
+            item,
+          );
         }
 
-        if (this.last15MinPrice < -1) {
-          sendEmailBySubject(`15分钟时间段出现下跌插针行情`, item);
+        if (lastPrice && +lastPrice.priceChangePercent < 1) {
+          sendEmailBySubject(`${formatTime(time)}时间段出现下跌插针行情`, item);
         }
       }
 
       if (price < -1) {
-        if (this.last15MinPrice < -1) {
-          sendEmailBySubject(`15分钟时间段已出现连续大波动下跌行情`, item);
+        if (lastPrice && +lastPrice.priceChangePercent < -1) {
+          sendEmailBySubject(
+            `${formatTime(time)}时间段已出现连续大波动下跌行情`,
+            item,
+          );
         }
 
-        if (this.last15MinPrice > 1) {
-          sendEmailBySubject(`15分钟时间段出现上涨插针行情`, item);
+        if (lastPrice && +lastPrice.priceChangePercent > 1) {
+          sendEmailBySubject(`${formatTime(time)}时间段出现上涨插针行情`, item);
         }
       }
 
-      this.last15MinPrice = price;
+      this.lastPrice[time] = price;
     }
   }
 
